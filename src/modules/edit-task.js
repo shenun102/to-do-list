1676e71837b3d7a325a1d9ae37956e68633eba5e
shenun102 <124623141+shenun102@users.noreply.github.com>
2024-08-31 13:29:44 +0100

    Clean up code, add list of features to implement

--- src/modules/edit-task.js
+++ src/modules/edit-task.js
@@ -1,36 +1,45 @@
-import { closeModal, openModal } from "./modal";
+import { openModal } from "./modal";
 import { setEditTaskHTML } from "./set-modal-html";
 import { allProjects, modalContainer } from "./initialize";
 import { editTarget } from "../index";
 
-
 export function editTask(target) {
-  console.log("Hello!", target.closest(".task-container"));
-
   setEditTaskHTML();
   openModal();
 }
 
 export function submitEdit(e, editTarget) {
-
   // First find the project the task belongs to
   const projectIndex = allProjects.findIndex(
     (project) => project.title === editTarget.dataset.project
   );
-  // Find index of task
-  const taskIndex = allProjects[projectIndex]
-    .loadTask()
-    .findIndex(
-      (task) =>
-        editTarget.querySelector(".task-title").textContent === task.title
-    );
 
-  const targetTask = allProjects[projectIndex].loadTask()[taskIndex];
+  if (projectIndex === -1) {
+    console.error("Project not found.");
+    return;
+  }
+
+  // get tasks array of project
+  const tasks = allProjects[projectIndex].loadTask();
+  // Find index of task
+  const taskIndex = tasks.findIndex(
+    (task) => editTarget.querySelector(".task-title").textContent === task.title
+  );
+
+  if (taskIndex === -1) {
+    console.error("Task not found.");
+    return;
+  }
+
+  const targetTask = tasks[taskIndex];
 
   // Get values from the form fields
-  targetTask.title = document.querySelector("#taskName").value;
-  targetTask.dueDate = document.querySelector("#task-deadline").value;
-  targetTask.priority = document.querySelector("#task-priority").value;
-  targetTask.desc = document.querySelector("#taskDescription").value;
+  const { value: title } = document.querySelector("#taskName");
+  const { value: dueDate } = document.querySelector("#task-deadline");
+  const { value: priority } = document.querySelector("#task-priority");
+  const { value: desc } = document.querySelector("#taskDescription");
+
+  Object.assign(targetTask, { title, dueDate, priority, desc });
+
   return allProjects[projectIndex];
 }
